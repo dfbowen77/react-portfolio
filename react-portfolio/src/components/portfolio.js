7dffb1e92e9df65667fa8bbfb478475746c9a92b
Dan Bowen <danbowen2677@gmail.com>
2023-06-13 16:14:24 -0400

    updated portfolio.js to include a second project

--- react-portfolio/src/components/portfolio.js
+++ react-portfolio/src/components/portfolio.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import project1pic from "../assets/profile-pic.jpg";
+import weatherDashboard from "../assets/Weather-dashboard.png";
+import textEditor from "../assets/Text-editor.png";
 import { Container, Row, Col, Image } from "react-bootstrap";
 
 function Portfolio () {
@@ -9,13 +11,13 @@ function Portfolio () {
             <h1 className="portfolio-title">Portfolio</h1>
             <Container>
                     <Row>
-                        <Col xs={12} md={4} className="d-flex justify-content-center align-items-center mb-4 mb-md-0">
-                        <h5 className="card-title">Project #1</h5>
+                        {/* <Col xs={12} md={4} className="d-flex justify-content-center align-items-center mb-4 mb-md-0"> */}
+                        <h5 className="card-title">Progressive Web Application Text Editor</h5>
                         <a href= "https://murmuring-reef-44441.herokuapp.com/" target="_blank" rel="noreferrer">
-                        <Image src={project1pic} rounded fluid thumbnail className='project1-pic'/>
+                        <Image src={textEditor} rounded fluid thumbnail className='project1-pic'/>
                         </a>
                         <div className="card-body">
-                            <p className="card-text">Project #1</p>
+                            <p className="card-text">Progressive Web Application Text Editor</p>
                             <a
                             href="https://murmuring-reef-44441.herokuapp.com/"
                             target="_blank"
@@ -33,7 +35,31 @@ function Portfolio () {
                             GitHub Repo
                             </a>
                         </div>
-                        </Col>
+                        {/* </Col> */}
+                        <h5 className="card-title">Weather Dashboard</h5>
+                        <a href= "https://dfbowen77.github.io/Weather-Dashboard/" target="_blank" rel="noreferrer">
+                        <Image src={weatherDashboard} rounded fluid thumbnail className='weatherDashboard-pic'/>
+                        </a>
+                        <div className="card-body">
+                            <p className="card-text">Weather Dashboard</p>
+                            <a
+                            href="https://dfbowen77.github.io/Weather-Dashboard/"
+                            target="_blank"
+                            rel="noreferrer"
+                            className="btn btn-deployed"
+                            >
+                            Deployed App
+                            </a>
+                            <a
+                            href="https://github.com/dfbowen77/Weather-Dashboard"
+                            target="_blank"
+                            rel="noreferrer"
+                            className="btn btn-repo"
+                            >
+                            GitHub Repo
+                            </a>
+                        </div>
+                        {/* </Col> */}
                     </Row>
                 </Container>
             </div>
