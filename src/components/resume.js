ca0b1571e0764946d1dd0e6eebd5c90390288d5e
Dan Bowen <danbowen2677@gmail.com>
2023-06-15 12:50:15 -0400

    updated styling on resume page

--- src/components/resume.js
+++ src/components/resume.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Container, Row, Col, Button } from "react-bootstrap";
+import { Container, Row, Col, Button, Stack } from "react-bootstrap";
 import resume from '../assets/DFB CV 2022.pdf'
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBootstrap, faCss3, faGit, faGithub, faHtml5, faJs, faNodeJs, faPython, faReact } from '@fortawesome/free-brands-svg-icons'
@@ -9,81 +9,116 @@ function Resume () {
     return (
         <section id='resume' className='resume-section'>
             <Container className='resume-container'>
-                <Row>
+                <Row className="my-5">
                     <Col>
+                    <Stack direction="horizontal" gap={5} className='d-flex justify-content-center'>
                         <h1>Resume</h1>
                         <Button href={resume} download>
-                            My Resume
+                            Download
                         </Button>
+                    </Stack>
+                    </Col>
+                </Row>
+                <Row>  
+                <Col  xs={12} className="d-flex align-items-center mb-4 p-4 mx-4 border border-dark border-5 rounded">
+                <Stack direction="horizontal" gap={5} className='d-flex justify-content-center'>
+                        <h2>Programming Skills</h2>
+                        <Stack gap={3} className='d-flex justify-content-center'>
+                                <h4>Languages</h4>
+                                <Stack direction="horizontal" gap={5} className='d-flex justify-content-center'> 
+                                <p>HTML <FontAwesomeIcon icon= {faHtml5}/> </p> 
+                                <p>CSS <FontAwesomeIcon icon= {faCss3}/></p> 
+                                <p>JavaScript <FontAwesomeIcon icon= {faJs}/></p>
+                                <p>SQL</p> 
+                                <p>SAS</p>
+                                <p>Python <FontAwesomeIcon icon= {faPython}/></p> 
+                                <p>R <FontAwesomeIcon icon= {faR}/></p>
+                                </Stack>
+                                <h4>Frameworks/Packages</h4>
+                                <Stack direction="horizontal" gap={5} className='d-flex justify-content-center'>
+                                    <p>React <FontAwesomeIcon icon= {faReact}/></p>
+                                    <p>Bootstrap <FontAwesomeIcon icon= {faBootstrap}/></p>
+                                    <p>Bulma</p>
+                                    <p>jQuery</p> 
+                                    <p>Django</p>
+                                    <p>Plotly</p>
+                                    <p>ggPlot2</p> 
+                                    <p>Express</p> 
+                                    <p>Node <FontAwesomeIcon icon= {faNodeJs}/></p>
+                                    <p>Lavaan</p>
+                                </Stack>
+                                <h4>Databases </h4>
+                                <Stack direction="horizontal" gap={5} className='d-flex justify-content-center'>
+                                    <p>MongoDB</p> 
+                                    <p>MongoDBCompass</p>
+                                    <p>MySQL</p> 
+                                    <p>MySQL Workbench</p> 
+                                    <p>Sqlite</p> 
+                                    <p>SQLite Studio</p> 
+                                </Stack>
+                                <h4>Other Platforms </h4>
+                                <Stack direction="horizontal" gap={5} className='d-flex justify-content-center'>
+                                    <p>Git <FontAwesomeIcon icon= {faGit}/></p>
+                                    <p>GitHub <FontAwesomeIcon icon= {faGithub}/></p>
+                                    <p>Heroku</p>
+                                    <p>Winsteps</p>
+                                    <p>flexMIRT</p>
+                                    <p>Insomnia</p>
+                                    <p>Anaconda</p>
+                                    <p>Spyder</p> 
+                                    <p>VSCode</p> 
+                                </Stack>
+                        </Stack>
+                    </Stack>
                     </Col>
                 </Row>
                 <Row>
-                    <Col>
+                    <Col  xs={12} className="d-flex align-items-center mb-4 p-4 mx-4 border border-dark border-5 rounded">
+                    <Stack direction="horizontal" gap={5} className='d-flex justify-content-center'>
                         <h2>Work Experience</h2>
-                        <ul className='work-list'>
-                            <li className='psychometrician'>
+                        <Stack gap={1} className='d-flex justify-content-center'>
                             <h4>Lead Psychometrician</h4>
                                 <h5>Measurement Inc, 2014-2022</h5>
                                     <p>
                                         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer cursus bibendum magna, vitae dapibus felis ultricies ut. Sed acodio eget est convallis ullamcorper. Duis bibendum vestibulum risus, non convallis odio aliquam eu.
                                     </p>
-                            </li>
-                            <li className='research-associate'>
                             <h4>Research Associate</h4>
                                 <h5>Measurement Inc, 2008-2014</h5>
                                     <p>
                                         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer cursus bibendum magna, vitae dapibus felis ultricies ut. Sed acodio eget est convallis ullamcorper. Duis bibendum vestibulum risus, non convallis odio aliquam eu.
                                     </p>
-                            </li>
-                            <li className='scoring-director'>
                             <h4>Scoring Director</h4>
                                 <h5>Measurement Inc, 2005-2008</h5>
                                     <p>
                                         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer cursus bibendum magna, vitae dapibus felis ultricies ut. Sed acodio eget est convallis ullamcorper. Duis bibendum vestibulum risus, non convallis odio aliquam eu.
-                                    </p> 
-                            </li>
-                        </ul>
-                        
-
-                        <h2>Programming Skills</h2>
-                        <ul className='web-development-skill-list'>
-                            <li className='languages'>
-                                Languages: HTML <FontAwesomeIcon icon= {faHtml5}/>, CSS <FontAwesomeIcon icon= {faCss3}/>, JavaScript <FontAwesomeIcon icon= {faJs}/>, SQL, SAS, Python <FontAwesomeIcon icon= {faPython}/>, R <FontAwesomeIcon icon= {faR}/>
-                            </li>
-                            <li className='frameworks'>
-                                Frameworks/Packages: React <FontAwesomeIcon icon= {faReact}/>, Bootstrap <FontAwesomeIcon icon= {faBootstrap}/>, Bulma , jQuery, Django, Plotly, ggPlot2, Express, Node <FontAwesomeIcon icon= {faNodeJs}/>, Lavaan
-                            </li>
-                            <li className='databases'>
-                                Databases: MongoDB, MongoDBCompass, MySQL, MySQL Workbench, Sqlite, SQLite Studio 
-                            </li>
-                            <li className='front-end-frameworks'>
-                                Other Platforms: Git <FontAwesomeIcon icon= {faGit}/>, GitHub <FontAwesomeIcon icon= {faGithub}/>, Heroku, Winsteps, flexMIRT, Insomnia
-                            </li>
-                        </ul>
+                                    </p>
+                            </Stack>
+                        </Stack>
+                    </Col>
+                </Row> 
+                
+                <Row>
+                <Col  xs={12} className="d-flex align-items-center mb-4 p-4 mx-4 border border-dark border-5 rounded">
+                <Stack direction="horizontal" gap={5} className='d-flex justify-content-center'>
                         <h2>Education</h2>
-                        <ul className='education-list'>
-                            <li className='uncg'>
+                        <Stack gap={2} className='d-flex justify-content-center'>
                             <h4>ABD in Educational Research Methodology</h4>
                                 <h5>University of North Carolina at Greensboro, 2012-2018</h5>
                                     <p>
                                         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer cursus bibendum magna, vitae dapibus felis ultricies ut. Sed acodio eget est convallis ullamcorper. Duis bibendum vestibulum risus, non convallis odio aliquam eu.
                                     </p>
-                            </li>
-                            <li className='unc-chapel-hill'>
                             <h4>Master of Arts in Educational Psychology Measurement and Evaluation</h4>
                                 <h5>University of North Carolina at Chapel Hill, 2007-2011</h5>
                                     <p>
                                         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer cursus bibendum magna, vitae dapibus felis ultricies ut. Sed acodio eget est convallis ullamcorper. Duis bibendum vestibulum risus, non convallis odio aliquam eu.
                                     </p>
-                            </li>
-                            <li className='unc-wilmington'>
                             <h4>Bachelor of Science in Operations Management and Information Systems</h4>
                                 <h5>University of North Carolina at Wilmington, 1998-2001</h5>
                                     <p>
                                         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer cursus bibendum magna, vitae dapibus felis ultricies ut. Sed acodio eget est convallis ullamcorper. Duis bibendum vestibulum risus, non convallis odio aliquam eu.
                                     </p> 
-                            </li>
-                        </ul>
+                        </Stack>
+                    </Stack>
                     </Col>
                 </Row>
             </Container>
